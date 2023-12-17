fb05a39632673f7085ed2f16a7775c578a666e77
Rizal Ilham <62590670+rizalilhamm@users.noreply.github.com>
2023-12-17 12:52:01 +0000

    Command usecase for SendMessage

--- modules/chats/usecases/command_usecase.go
+++ modules/chats/usecases/command_usecase.go
@@ -5,20 +5,22 @@ import (
     "jubelio.com/chat/migrations/models"
     "jubelio.com/chat/packages/utils"
 
-    "github.com/go-playground/validator/v10"
+    // "github.com/go-playground/validator/v10"
 )
 
-func SendMessage(ctx context.Context, chatPayload models.Chats, messagePayload models.Messages) utils.Result {
-    validate := validator.New()
+func (u usecase) SendMessage(ctx context.Context, chatPayload models.Chats, messagePayload models.Messages) utils.Result {
+    // validate := validator.New()
 	var result utils.Result
 
-    if err := validate.Struct(chatPayload); err != nil {
-        return result
-    }
+    // if err := validate.Struct(chatPayload); err != nil {
+    //     return result
+    // }
 	
-    if err := validate.Struct(messagePayload); err != nil {
-        return result
-    }
+    // if err := validate.Struct(messagePayload); err != nil {
+    //     return result
+    // }
+
+    result.Data = "Berhasil Running SendMessage Method"
 
     return result
 }
