ae49f49f63814da75d9377ed06d641d8d60b07da
Rizal Ilham <62590670+rizalilhamm@users.noreply.github.com>
2023-12-17 12:52:32 +0000

    Add handler in main

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"jubelio.com/chat/modules/chats/handlers"
 )
 
 func main() {
@@ -12,6 +13,11 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	handler := handlers.New()
+
+	// Mount routes from your HTTPHandler to the Fiber app
+	handler.Mount(app)
 
 	// Start the Fiber server on port 3000
 	err := app.Listen(":3000")
